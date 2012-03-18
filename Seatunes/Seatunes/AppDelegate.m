18baa83376dd8d6e4ac3cd791a2b302b70eeafa3
Paul Vishayanuroj <pvishayanuroj@gmail.com>
2012-03-18 10:01:52 -0400

    Changed default song

--- Seatunes/Seatunes/AppDelegate.m
+++ Seatunes/Seatunes/AppDelegate.m
@@ -115,7 +115,7 @@
 	// Run the intro Scene
     [glView setMultipleTouchEnabled:YES];
 	//[[CCDirector sharedDirector] runWithScene: [MainMenuScene node]];
-    [[CCDirector sharedDirector] runWithScene: [GameScene startWithDifficulty:kDifficultyMedium songName:@"Three Blind Mice"]];
+    [[CCDirector sharedDirector] runWithScene: [GameScene startWithDifficulty:kDifficultyMedium songName:@"Twinkle Twinkle"]];
 }
 
 
